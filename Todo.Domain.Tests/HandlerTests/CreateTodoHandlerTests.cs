bed80b655bfc31d8c0c89c81ac246e9214923345
carlosabsoares <carlosabsoares@gmail.com>
2020-06-19 19:38:10 -0300

    Aula_28

--- Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -1,25 +1,49 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Reflection.Metadata;
 using Todo.Domain.Commands;
 using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
 
 namespace Todo.Domain.Tests.HandlerTests
 {
     [TestClass]
     public class CreateTodoHandlerTests
     {
-        [TestMethod]
-        public void Dado_um_comando_invalido_deve_interromper_a_execucao()
+
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Titulo da tarefa", "Carlos Soares", DateTime.Now);
+
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+
+        private GenericCommandResult _result = new GenericCommandResult();
+
+        public CreateTodoHandlerTests()
         {
-            var command = new CreateTodoCommand("","",DateTime.Now);
-            var handler = new TodoHandler(null);
-            Assert.Fail();
+            _invalidCommand.Validate();
+            _validCommand.Validate();
         }
 
         [TestMethod]
-        public void Dado_um_comando_invalido_deve_criar_a_tarefa()
+        public void Dado_um_comando_invalido_deve_interromper_a_execucao()
         {
-            Assert.Fail();
+            
+            var handler = new TodoHandler(new FakeTodoRepository());
+
+            _result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+
+            Assert.AreEqual(_result.Success , false);
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido_deve_criar_a_tarefa()
+        {
+            
+            var handler = new TodoHandler(new FakeTodoRepository());
+
+            _result = (GenericCommandResult)_handler.Handle(_validCommand);
+
+            Assert.AreEqual(_result.Success, true);
         }
     }
 }
